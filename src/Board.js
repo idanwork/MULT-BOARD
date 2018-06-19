4f0d7619f4282e7c4c0cd1c28ada8847220cae23
= <=idanmz@waves.com>
2018-06-19 14:12:52 +0300

    added comments

--- src/Board.js
+++ src/Board.js
@@ -14,11 +14,14 @@ class Board extends Component {
   buildTable(){
     let rows = [[0]];
 
+    // Create a "fake" row for the titles
     for(let j = 1; j <= this.props.size; j++){
         rows[0].push(j);
     }
 
+    // Create the table values 
     for(let i =1; i<= this.props.size; i++){
+        // Create a "fake" column for the titles 
         let row = [i];
 
         for(let j = 1; j <= this.props.size; j++){
